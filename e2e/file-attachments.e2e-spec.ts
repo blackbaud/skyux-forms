a2501446de4364cafb1f60a8f21f0a3dd9a238b7
Alex <alex.kingman@blackbaud.com>
2018-10-02 09:31:17 -0400

    Initial port of form components (#1)

---
+++ e2e/file-attachments.e2e-spec.ts
@@ -0,0 +1,37 @@
+import {
+  element,
+  by
+} from 'protractor';
+
+import {
+  expect,
+  SkyHostBrowser
+} from '@skyux-sdk/e2e';
+
+describe('File attachments', () => {
+  it('should match previous file attachments screenshot', (done) => {
+    SkyHostBrowser.get('visual/file-attachment');
+    SkyHostBrowser.setWindowBreakpoint('lg');
+    expect('#screenshot-file-drop').toMatchBaselineScreenshot(done);
+  });
+
+  it('should match previous file attachments screenshot (screen: xs)', (done) => {
+    SkyHostBrowser.get('visual/file-attachment');
+    SkyHostBrowser.setWindowBreakpoint('xs');
+    expect('#screenshot-file-drop').toMatchBaselineScreenshot(done);
+  });
+
+  it('should match previous file attachments screenshot when no links allowed', (done) => {
+    SkyHostBrowser.get('visual/file-attachment');
+    SkyHostBrowser.setWindowBreakpoint('lg');
+    element(by.css('.sky-test-allow-links')).click();
+    expect('#screenshot-file-drop').toMatchBaselineScreenshot(done);
+  });
+
+  it('should match previous file attachments screenshot (screen: xs)', (done) => {
+    SkyHostBrowser.get('visual/file-attachment');
+    SkyHostBrowser.setWindowBreakpoint('xs');
+    element(by.css('.sky-test-allow-links')).click();
+    expect('#screenshot-file-drop').toMatchBaselineScreenshot(done);
+  });
+});
