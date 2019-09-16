6a96cd5a0675b44755b018d894ed595b6d17c1ed
Erika McVey <erika.mcvey@blackbaud.com>
2019-09-16 15:15:27 -0400

    try running e2e tests without single file attachement

--- e2e/file-attachments.e2e-spec.ts
+++ e2e/file-attachments.e2e-spec.ts
@@ -8,10 +8,6 @@ import {
   SkyHostBrowser
 } from '@skyux-sdk/e2e';
 
-import {
-  SkyHostBrowserBreakpoint
-} from '@skyux-sdk/e2e/host-browser/host-browser-breakpoint';
-
 describe('File attachments', () => {
   it('should match previous file drop screenshot', (done) => {
     SkyHostBrowser.get('visual/file-attachment');
@@ -38,52 +34,12 @@ describe('File attachments', () => {
     });
   });
 
-  it('should match previous file drop screenshot when no links allowed (screen: xs)', (done) => {
+  it('should match previous file drop screenshot (screen: xs)', (done) => {
     SkyHostBrowser.get('visual/file-attachment');
     SkyHostBrowser.setWindowBreakpoint('xs');
     element(by.css('.sky-test-allow-links')).click();
     expect('#screenshot-file-drop').toMatchBaselineScreenshot(done, {
       screenshotName: 'file-drop-no-links-xs'
-    });
-  });
-
-  describe('with label set', () => {
-    function matchesPreviousAttachmentAndLabel(screenSize: SkyHostBrowserBreakpoint, done: DoneFn): void {
-      SkyHostBrowser.get('visual/file-attachment');
-      SkyHostBrowser.setWindowBreakpoint(screenSize);
-
-      expect('#screenshot-file-attachment').toMatchBaselineScreenshot(done, {
-        screenshotName: `file-attachment-label-${screenSize}`
-      });
-    }
-
-    it('should match previous screenshot on large screens', (done) => {
-      matchesPreviousAttachmentAndLabel('lg', done);
-    });
-
-    it('should match previous screenshot on small screens', (done) => {
-      matchesPreviousAttachmentAndLabel('xs', done);
-    });
-  });
-
-  describe('without label set', () => {
-    function matchesPreviousAttachmentAndLabel(screenSize: SkyHostBrowserBreakpoint, done: DoneFn): void {
-      SkyHostBrowser.get('visual/file-attachment');
-      SkyHostBrowser.setWindowBreakpoint(screenSize);
-
-      element(by.css('.sky-test-label')).click();
-
-      expect('#screenshot-file-attachment').toMatchBaselineScreenshot(done, {
-        screenshotName: `file-attachment-no-label-${screenSize}`
-      });
-    }
-
-    it('should match previous screenshot on large screens', (done) => {
-      matchesPreviousAttachmentAndLabel('lg', done);
-    });
-
-    it('should match previous screenshot on small screens', (done) => {
-      matchesPreviousAttachmentAndLabel('xs', done);
     });
   });
 });
