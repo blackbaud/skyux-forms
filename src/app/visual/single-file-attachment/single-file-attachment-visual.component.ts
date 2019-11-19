46a400b18ecba4e2b6390d3d38b44b23fad019a7
Alex Kingman <alex.kingman@blackbaud.com>
2019-11-19 17:10:43 -0500

    Added disabled input property for file attachment component

--- src/app/visual/single-file-attachment/single-file-attachment-visual.component.ts
+++ src/app/visual/single-file-attachment/single-file-attachment-visual.component.ts
@@ -19,9 +19,12 @@ import {
   templateUrl: './single-file-attachment-visual.component.html'
 })
 export class SingleFileAttachmentVisualComponent implements OnInit {
+
   public acceptedTypes: Array<String>;
 
   public attachment: FormControl;
+
+  public disabled: boolean = false;
 
   public fileForm: FormGroup;
 
@@ -61,6 +64,15 @@ export class SingleFileAttachmentVisualComponent implements OnInit {
 
   public deleteFile(file: SkyFileItem | SkyFileLink): void {
     this.removeFromArray(this.filesToUpload, file);
+  }
+
+  public onToggleDisabledClick(): void {
+    this.disabled = !this.disabled;
+    if (this.disabled) {
+      this.attachment.disable();
+    } else {
+      this.attachment.enable();
+    }
   }
 
   private removeFromArray(items: Array<any>, obj: SkyFileItem | SkyFileLink): void {
