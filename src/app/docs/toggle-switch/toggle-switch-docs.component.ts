8d54e8bb514bb38bd15e0bd9bee53fc3b9c60aa7
Somalia Jamall <somalia.jamall@blackbaud.com>
2020-04-15 09:28:39 -0500

    Clean up code for e2e check

--- src/app/docs/toggle-switch/toggle-switch-docs.component.ts
+++ src/app/docs/toggle-switch/toggle-switch-docs.component.ts
@@ -14,25 +14,28 @@ import {
 
 @Component({
   selector: 'app-toggle-switch-docs',
-  templateUrl: './toggle-switch-docs.component.html',
+  templateUrl: './toggle-switch-docs.component.html'
 })
 
 export class ToggleSwitchComponent {
   public demoSettings: any = {};
-  public switchLable = "Inactive";
+  public switchLable = 'Inactive';
   public showLable = true;
+  public checked = false;
 
   public get switchStatus(): 'Active' | 'Inactive' {
     return (this.checked === true) ? 'Active' : 'Inactive';
   }
 
   public onDemoSelectionChange(change: SkyDocsDemoControlPanelChange): void {
-    if (change.toggleLabel !== undefined) this.showLable = change.toggleLabel;
+    if (change.toggleLabel !== undefined) {
+      this.showLable = change.toggleLabel;
+    }
 
     if (this.showLable === true) {
       this.switchLable = this.switchStatus;
     }
-    else if (this.showLable === false) {
+    if (this.showLable === false) {
       this.switchLable = ' ';
     }
   }
