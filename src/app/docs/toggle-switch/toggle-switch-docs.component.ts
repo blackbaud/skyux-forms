8fa6c76c0766bbccb73242ea3aef9fadc15fa285
Somalia Jamall <somalia.jamall@blackbaud.com>
2020-04-15 08:55:56 -0500

    Add design demo

--- src/app/docs/toggle-switch/toggle-switch-docs.component.ts
+++ src/app/docs/toggle-switch/toggle-switch-docs.component.ts
@@ -1,16 +1,39 @@
 import {
-  ChangeDetectionStrategy,
   Component
 } from '@angular/core';
 
 import {
-  SkyDocsDemoControlPanelChange,
-  SkyDocsDemoControlPanelRadioChoice
+  SkyDocsDemoControlPanelChange
 } from '@skyux/docs-tools';
+
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup
+} from '@angular/forms';
 
 @Component({
   selector: 'app-toggle-switch-docs',
   templateUrl: './toggle-switch-docs.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ToggleSwitchDocsComponent { }
+
+export class ToggleSwitchComponent {
+  public demoSettings: any = {};
+  public switchLable = "Inactive";
+  public showLable = true;
+
+  public get switchStatus(): 'Active' | 'Inactive' {
+    return (this.checked === true) ? 'Active' : 'Inactive';
+  }
+
+  public onDemoSelectionChange(change: SkyDocsDemoControlPanelChange): void {
+    if (change.toggleLabel !== undefined) this.showLable = change.toggleLabel;
+
+    if (this.showLable === true) {
+      this.switchLable = this.switchStatus;
+    }
+    else if (this.showLable === false) {
+      this.switchLable = ' ';
+    }
+  }
+}
