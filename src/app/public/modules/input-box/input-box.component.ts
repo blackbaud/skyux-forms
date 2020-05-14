e71f6880d2a59c40b92346516a32e1da17653ac9
Paul Crowder <paul.crowder@blackbaud.com>
2020-05-14 09:44:43 -0400

    Commit before 4.x merge

---
+++ src/app/public/modules/input-box/input-box.component.ts
@@ -0,0 +1,12 @@
+import {
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'sky-input-box',
+  templateUrl: './input-box.component.html',
+  styleUrls: ['./input-box.component.scss']
+})
+export class SkyInputBoxComponent {
+
+}
---
+++ src/app/visual/input-box/input-box-visual.component.ts
@@ -0,0 +1,12 @@
+import {
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'app-input-box-visual',
+  templateUrl: './input-box-visual.component.html',
+  styleUrls: ['./input-box-visual.component.scss']
+})
+export class InputBoxVisualComponent {
+
+}
