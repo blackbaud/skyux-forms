0bbce0390542edfa559a3335e0af59d5a8dd453c
John Lyons <John.Lyons@blackbaud.me>
2020-05-16 00:57:19 -0400

    Phase 2 docs for character count

---
+++ src/app/docs/character-count/character-count-docs.component.ts
@@ -0,0 +1,9 @@
+import {
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'app-character-count-docs',
+  templateUrl: './character-count-docs.component.html'
+})
+export class CharacterCountDocsComponent { }
