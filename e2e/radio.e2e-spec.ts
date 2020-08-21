26daca61f5a5bcd72359e01641e78580c9bf27ee
Paul Crowder <paul.crowder@blackbaud.com>
2020-08-21 12:06:33 -0400

    Added modern theme styles to checkbox and radio button

--- e2e/radio.e2e-spec.ts
+++ e2e/radio.e2e-spec.ts
@@ -1,43 +1,94 @@
 import {
   expect,
-  SkyHostBrowser
+  SkyHostBrowser,
+  SkyVisualThemeSelector
 } from '@skyux-sdk/e2e';
 
 describe('Radio', () => {
+  let currentTheme: string;
+  let currentThemeMode: string;
 
-  it('should match previous radio screenshot', (done) => {
-    SkyHostBrowser.get('visual/radio');
-    SkyHostBrowser.setWindowBreakpoint('lg');
-    SkyHostBrowser.scrollTo('#screenshot-radio');
-    expect('#screenshot-radio').toMatchBaselineScreenshot(done, {
-      screenshotName: 'radio'
+  async function selectTheme(theme: string, mode: string): Promise<void> {
+    currentTheme = theme;
+    currentThemeMode = mode;
+
+    return SkyVisualThemeSelector.selectTheme(theme, mode);
+  }
+
+  function getScreenshotName(name: string): string {
+    if (currentTheme) {
+      name += '-' + currentTheme;
+    }
+
+    if (currentThemeMode) {
+      name += '-' + currentThemeMode;
+    }
+
+    return name;
+  }
+
+  function validateAll() {
+    it('should match previous radio screenshot', async (done) => {
+      await SkyHostBrowser.setWindowBreakpoint('lg');
+      await SkyHostBrowser.scrollTo('#screenshot-radio');
+
+      expect('#screenshot-radio').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('radio')
+      });
     });
+
+    it('should match previous radio screenshot (screen: xs)', async (done) => {
+      await SkyHostBrowser.setWindowBreakpoint('xs');
+      await SkyHostBrowser.scrollTo('#screenshot-radio');
+
+      expect('#screenshot-radio').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('radio-xs')
+      });
+    });
+
+    it('should match previous icon radio screenshot', async (done) => {
+      await SkyHostBrowser.setWindowBreakpoint('lg');
+      await SkyHostBrowser.scrollTo('#screenshot-icon-radio');
+
+      expect('#screenshot-icon-radio').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('icon-radio')
+      });
+    });
+
+    it('should match previous icon radio screenshot (screen: xs)', async (done) => {
+      await SkyHostBrowser.setWindowBreakpoint('xs');
+      await SkyHostBrowser.scrollTo('#screenshot-icon-radio');
+
+      expect('#screenshot-icon-radio').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('icon-radio-xs')
+      });
+    });
+  }
+
+  beforeEach(async () => {
+    await SkyHostBrowser.get('visual/radio');
   });
 
-  it('should match previous radio screenshot (screen: xs)', (done) => {
-    SkyHostBrowser.get('visual/radio');
-    SkyHostBrowser.setWindowBreakpoint('xs');
-    SkyHostBrowser.scrollTo('#screenshot-radio');
-    expect('#screenshot-radio').toMatchBaselineScreenshot(done, {
-      screenshotName: 'radio-xs'
+  validateAll();
+
+  describe('when modern theme', () => {
+
+    beforeEach(async () => {
+      await selectTheme('modern', 'light');
     });
+
+    validateAll();
+
   });
 
-  it('should match previous icon radio screenshot', (done) => {
-    SkyHostBrowser.get('visual/radio');
-    SkyHostBrowser.setWindowBreakpoint('lg');
-    SkyHostBrowser.scrollTo('#screenshot-icon-radio');
-    expect('#screenshot-icon-radio').toMatchBaselineScreenshot(done, {
-      screenshotName: 'icon-radio'
+  describe('when modern theme in dark mode', () => {
+
+    beforeEach(async () => {
+      await selectTheme('modern', 'dark');
     });
+
+    validateAll();
+
   });
 
-  it('should match previous icon radio screenshot (screen: xs)', (done) => {
-    SkyHostBrowser.get('visual/radio');
-    SkyHostBrowser.setWindowBreakpoint('xs');
-    SkyHostBrowser.scrollTo('#screenshot-icon-radio');
-    expect('#screenshot-icon-radio').toMatchBaselineScreenshot(done, {
-      screenshotName: 'icon-radio-xs'
-    });
-  });
 });
