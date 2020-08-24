0aa11d9cf8404626467f7d8f6c57e19e4a04d445
Paul Crowder <paul.crowder@blackbaud.com>
2020-08-24 10:19:52 -0400

    Added modern theme styles to checkbox and radio button (#173)
    * Added modern theme styles to checkbox and radio button
    
    * Fix checkbox visual tests

--- e2e/checkbox.e2e-spec.ts
+++ e2e/checkbox.e2e-spec.ts
@@ -1,39 +1,90 @@
 import {
   expect,
-  SkyHostBrowser
+  SkyHostBrowser,
+  SkyVisualThemeSelector
 } from '@skyux-sdk/e2e';
 
 describe('Checkbox', () => {
+  let currentTheme: string;
+  let currentThemeMode: string;
 
-  it('should match previous checkbox screenshot', (done) => {
-    SkyHostBrowser.get('visual/checkbox');
-    SkyHostBrowser.setWindowBreakpoint('lg');
-    expect('#screenshot-checkbox').toMatchBaselineScreenshot(done, {
-      screenshotName: 'checkbox'
+  async function selectTheme(theme: string, mode: string): Promise<void> {
+    currentTheme = theme;
+    currentThemeMode = mode;
+
+    return SkyVisualThemeSelector.selectTheme(theme, mode);
+  }
+
+  function getScreenshotName(name: string): string {
+    if (currentTheme) {
+      name += '-' + currentTheme;
+    }
+
+    if (currentThemeMode) {
+      name += '-' + currentThemeMode;
+    }
+
+    return name;
+  }
+
+  function validateAll() {
+    it('should match previous checkbox screenshot', async (done) => {
+      await SkyHostBrowser.setWindowBreakpoint('lg');
+
+      expect('#screenshot-checkbox').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('checkbox')
+      });
     });
+
+    it('should match previous checkbox screenshot (screen: xs)', async (done) => {
+      await SkyHostBrowser.setWindowBreakpoint('xs');
+
+      expect('#screenshot-checkbox').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('checkbox-xs')
+      });
+    });
+
+    it('should match previous icon checkbox screenshot', async (done) => {
+      await SkyHostBrowser.setWindowBreakpoint('lg');
+
+      expect('#screenshot-icon-checkbox').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('checkbox-icon')
+      });
+    });
+
+    it('should match previous icon checkbox screenshot (screen: xs)', async (done) => {
+      await SkyHostBrowser.setWindowBreakpoint('xs');
+
+      expect('#screenshot-icon-checkbox').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('checkbox-icon-xs')
+      });
+    });
+  }
+
+  beforeEach(async () => {
+    await SkyHostBrowser.get('visual/checkbox');
   });
 
-  it('should match previous checkbox screenshot (screen: xs)', (done) => {
-    SkyHostBrowser.get('visual/checkbox');
-    SkyHostBrowser.setWindowBreakpoint('xs');
-    expect('#screenshot-checkbox').toMatchBaselineScreenshot(done, {
-      screenshotName: 'checkbox-xs'
+  validateAll();
+
+  describe('when modern theme', () => {
+
+    beforeEach(async () => {
+      await selectTheme('modern', 'light');
     });
+
+    validateAll();
+
   });
 
-  it('should match previous icon checkbox screenshot', (done) => {
-    SkyHostBrowser.get('visual/checkbox');
-    SkyHostBrowser.setWindowBreakpoint('lg');
-    expect('#screenshot-icon-checkbox').toMatchBaselineScreenshot(done, {
-      screenshotName: 'checkbox-icon'
+  describe('when modern theme in dark mode', () => {
+
+    beforeEach(async () => {
+      await selectTheme('modern', 'dark');
     });
+
+    validateAll();
+
   });
 
-  it('should match previous icon checkbox screenshot (screen: xs)', (done) => {
-    SkyHostBrowser.get('visual/checkbox');
-    SkyHostBrowser.setWindowBreakpoint('xs');
-    expect('#screenshot-icon-checkbox').toMatchBaselineScreenshot(done, {
-      screenshotName: 'checkbox-icon-xs'
-    });
-  });
 });
