c01f66493c9f546c1e477d4bbc393f8dbbf4528f
Alex Kingman <alex.kingman@blackbaud.me>
2020-09-03 11:42:17 -0400

    Demos for checkbox component

--- src/app/docs/checkbox/checkbox-docs.component.ts
+++ src/app/docs/checkbox/checkbox-docs.component.ts
@@ -6,4 +6,41 @@ import {
   selector: 'app-checkbox-docs',
   templateUrl: './checkbox-docs.component.html'
 })
-export class CheckboxDocsComponent { }
+export class CheckboxDocsComponent {
+
+  public checkboxItems: any[] = [
+    {
+      label: 'Selected checkbox',
+      checked: true,
+      disabled: false
+    },
+    {
+      label: 'Unselected checkbox',
+      checked: false,
+      disabled: false
+    },
+    {
+      label: 'This checkbox cannot be selected right now',
+      disabled: true
+    }
+  ];
+
+  public iconCheckboxGroup: any[] = [
+    {
+      label: 'Bold',
+      checked: true,
+      icon: 'bold'
+    },
+    {
+      label: 'Italic',
+      checked: false,
+      icon: 'italic'
+    },
+    {
+      label: 'Underline',
+      checked: false,
+      icon: 'underline'
+    }
+  ];
+
+}
---
+++ src/app/public/plugin-resources/code-examples/checkbox/checkbox-demo.component.ts
@@ -0,0 +1,79 @@
+import {
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'sky-checkbox-demo',
+  templateUrl: './checkbox-demo.component.html'
+})
+export class CheckboxDemoComponent {
+
+  public checkboxItems: any[] = [
+    {
+      label: 'Checkbox 1'
+    },
+    {
+      label: 'Checkbox 2',
+      checked: true
+    },
+    {
+      label: 'Disabled',
+      disabled: true
+    },
+    {
+      label: 'Disabled and selected',
+      checked: true,
+      disabled: true
+    }
+  ];
+
+  public iconCheckboxGroup: any[] = [
+    {
+      label: 'Bold',
+      checked: true,
+      icon: 'bold'
+    },
+    {
+      label: 'Italic',
+      checked: false,
+      icon: 'italic'
+    },
+    {
+      label: 'Underline',
+      checked: true,
+      icon: 'underline'
+    }
+  ];
+
+  public iconCheckboxItems: any[] = [
+    {
+      label: 'Info icon checkbox 1',
+      checked: true,
+      icon: 'info'
+    },
+    {
+      label: 'Disabled info icon checkbox 1',
+      checked: true,
+      disabled: true,
+      icon: 'strikethrough'
+    },
+    {
+      label: 'Success icon checkbox',
+      checked: true,
+      icon: 'star',
+      checkboxType: 'success'
+    },
+    {
+      label: 'Warning icon checkbox',
+      checked: true,
+      icon: 'exclamation-triangle',
+      checkboxType: 'warning'
+    },
+    {
+      label: 'Danger icon checkbox',
+      checked: true,
+      icon: 'ban',
+      checkboxType: 'danger'
+    }
+  ];
+}
