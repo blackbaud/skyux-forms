b43de449c8313317274efbe41ba3c408b894aad7
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2021-09-02 15:43:37 -0400

    Partial mode

--- projects/forms/src/public-api.ts
+++ projects/forms/src/public-api.ts
@@ -25,3 +25,26 @@ export * from './modules/selection-box/selection-box.module';
 
 export * from './modules/toggle-switch/types/toggle-switch-change';
 export * from './modules/toggle-switch/toggle-switch.module';
+
+// Components and directives must be exported to support Angular's "partial" Ivy compiler.
+// Obscure names are used to indicate types are not part of the public API.
+
+export { SkyCharacterCounterIndicatorComponent as λ1 } from './modules/character-counter/character-counter-indicator.component';
+export { SkyCharacterCounterInputDirective as λ2 } from './modules/character-counter/character-counter.directive';
+export { SkyCheckboxComponent as λ3 } from './modules/checkbox/checkbox.component';
+export { SkyCheckboxLabelComponent as λ4 } from './modules/checkbox/checkbox-label.component';
+export { SkyCheckboxRequiredValidatorDirective as λ5 } from './modules/checkbox/checkbox-required-validator.directive';
+export { SkyFileAttachmentLabelComponent as λ6 } from './modules/file-attachment/file-attachment-label.component';
+export { SkyFileAttachmentComponent as λ7 } from './modules/file-attachment/file-attachment.component';
+export { SkyFileDropComponent as λ8 } from './modules/file-attachment/file-drop.component';
+export { SkyFileItemComponent as λ9 } from './modules/file-attachment/file-item.component';
+export { SkyInputBoxComponent as λ10 } from './modules/input-box/input-box.component';
+export { SkyRadioGroupComponent as λ11 } from './modules/radio/radio-group.component';
+export { SkyRadioLabelComponent as λ12 } from './modules/radio/radio-label.component';
+export { SkyRadioComponent as λ13 } from './modules/radio/radio.component';
+export { SkySelectionBoxDescriptionComponent as λ14 } from './modules/selection-box/selection-box-description.component';
+export { SkySelectionBoxGridComponent as λ15 } from './modules/selection-box/selection-box-grid.component';
+export { SkySelectionBoxHeaderComponent as λ16 } from './modules/selection-box/selection-box-header.component';
+export { SkySelectionBoxComponent as λ17 } from './modules/selection-box/selection-box.component';
+export { SkyToggleSwitchLabelComponent as λ18 } from './modules/toggle-switch/toggle-switch-label.component';
+export { SkyToggleSwitchComponent as λ19 } from './modules/toggle-switch/toggle-switch.component';
